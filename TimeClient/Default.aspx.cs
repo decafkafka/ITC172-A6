e64fe2fc3b1f76a30cb43a99bc3629a4a8500945
decafkafka <yhan.ccs@gmail.com>
2015-03-11 18:42:08 -0700

    First Upload of Assignment

---
+++ TimeClient/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Timer1.Enabled = true;
+    }
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        TimeService.ItimeServiceClient ts = new TimeService.ItimeServiceClient();
+        //Timer1.Interval = 1000;
+        lblTime.Text = ts.GetCurrentTime();
+    }
+}
